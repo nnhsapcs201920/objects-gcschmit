fc5e0e3bd552c979c9752930257dc39b8a42281d
Geoff Schmit <gcschmit@gmail.com>
2019-08-20 09:32:56 -0500

    started introduction to Java
    defined class, method, statements
    need to start with indentifiers tomorrow

---
+++ ClassNotes/HelloPrinter.java
@@ -0,0 +1,42 @@
+
+/**
+ * Every Java program contains one or more classes (e.g., HelloPrinter).
+ * 
+ * In general, every source file (e.g., HelloPrinter.java) contains one class (e.g., HelloPrinter).
+ * 
+ * The source file name must match the class name.
+ * 
+ * By convention, class names start with an uppercase letter.
+ *
+ * @author gcschmit
+ * @version 20 August 2019
+ */
+public class HelloPrinter
+{
+    /*
+     * A class contains methods.
+     * 
+     * Most Java applications contain a class with a main method.
+     * 
+     * The main method is executed when the application starts.
+     */
+    public static void main(String[] args)
+    {
+        /*
+         * A method contains statements.
+         * 
+         * Statements may invoke other methods (e.g., println).
+         * 
+         * In general, methods are invoked on objects (e.g., System.out).
+         * 
+         * When invoking a method, arguments are passed in parentheses (e.g., "Hello, World!").
+         */
+        System.out.println("Hello, World!");    // comment at the end of a line
+        
+        // this is a single-line comment (like # python)
+    }
+}
+
+
+
+
